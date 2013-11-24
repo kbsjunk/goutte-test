16cd05e85bda0478aa0d3170ed787a3690cda06b
KitBS <kit.burton.senior@gmail.com>
2013-11-24 21:00:27 +1100

    more scrape details

--- functions.php
+++ functions.php
@@ -3,30 +3,73 @@
 class EurovisionCrawler {
 
 	private static $patterns = array(
-		'city year' => '(?P<city>.*) (?P<year>[0-9]{4})'
+		'city year' => '(?P<city>.*) (?P<year>[0-9]{4})',
+		'pt goes to' => '(?P<points>[0-9]*)pt from (?P<from>.*) goes to (?P<to>.*)',
+		'youtube' => 'v=(?P<youtube>[^&]*)',
+		'song video' => '(?P<song>.*?)\s*Watch video',
+		'location city country' => '(?P<venue>.*), *(?P<city>.*), *(?P<country>.*)',
+		'performer from country' => '(?P<performer>.*?) *from *(?P<country>.*)?',
 		);
 
 
 	public static function extract($text, $pattern) {
 
-		preg_match_all(\EurovisionCrawler::_pattern($pattern), $text, $result);
+		$pattern = \EurovisionCrawler::_pattern($pattern);
+
+		if (!$pattern) return $text;
+
+		preg_match_all($pattern, trim($text), $result);
+
 		$return = new stdClass();
-		foreach ($result as $key => $value)
+
+		foreach ($result as $key => $value) {
 			if (!is_numeric($key)) {
-				$return->$key = $value[0];
+				$return->$key = trim($value[0]);
 			}
-			return $return;
 		}
 
-		private static function _pattern($key) {
+		return $return;
+	}
 
-			if (isset(\EurovisionCrawler::$patterns[$key])) {
-				return '/'.\EurovisionCrawler::$patterns[$key].'/';
-			}
-			else {
-				return false;
-			}
+	private static function _pattern($key) {
 
+		if (isset(\EurovisionCrawler::$patterns[$key])) {
+			return '/'.\EurovisionCrawler::$patterns[$key].'/';
 		}
+		else {
+			return false;
+		}
+	}
+
+	public static function split($text, $delimiter = ',') {
+
+		$text = explode($delimiter, trim(trim($text), $delimiter));
+
+		array_walk($text, function(&$n) {
+			$n = trim($n);
+		}); 
+
+		return $text;
 
 	}
+
+	public static function liftArray($array) {
+		$return = array();
+
+		foreach ($array as $id => $subarray) {
+			foreach ($subarray as $key => $value) {
+				$return[$key] = $value;
+			}
+		}
+
+		return $return;
+
+	}
+
+}
+
+class EurovisionEvent {
+
+
+
+}
--- index.php
+++ index.php
@@ -10,46 +10,161 @@ use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
+$ev = new \EurovisionEvent();
 
 // --------------------------------------------------- //
 
-$crawler = $client->request('GET', 'http://www.eurovision.tv/page/history/year');
+// $crawler = $client->request('GET', 'http://www.eurovision.tv/page/history/year');
 
-$contests = $crawler->filter('.event-entry a')->each(function (Crawler $node, $i) {
+// $contests = $crawler->filter('.event-entry a')->each(function (Crawler $node, $i) {
 
-	$city = \EurovisionCrawler::extract($node->text(), 'city year');
+// 	$city = \EurovisionCrawler::extract($node->text(), 'city year');
 
-	return array(
-		'city' => $city->city,
-		'year' => $city->year,
-		'link' => $node->link()->getUri()
-		);
-});
+// 	return array(
+// 		'city' => $city->city,
+// 		'year' => $city->year,
+// 		'link' => $node->link()->getUri()
+// 		);
+// });
 
-print_r($contests);
+// print_r($contests);
 
 // --------------------------------------------------- //
 
 // 
 
-$crawler = $client->request('GET', 'http://www.eurovision.tv/page/history/by-year/contest?event=273');
+$crawler = $client->request('GET', 'http://www.eurovision.tv/page/history/by-year/contest?event=1543');//273
 
-$details = $crawler->filter('.cb-EventInfo-facts .detail-list')->filter('h3, p.info')->each(function (Crawler $node, $i) {
+$details = $crawler->filter('.cb-EventInfo-facts .detail-list')->filter('h3')->each(function (Crawler $node, $i) {
 
-return $node->text();
+	$key = trim($node->text());
+	$value = trim($node->nextAll()->first()->text());//filter('p.info, ul.qualifiers')->
 
-	//$city = \EurovisionCrawler::extract($node->text(), 'city year');
+	switch ($key) {
 
-	// return array(
-	// 	'city' => $city->city,
-	// 	'year' => $city->year,
-	// 	'link' => $node->link()->getUri()
-	// 	);
+		case 'Location':
+		$value = \EurovisionCrawler::extract($value, 'location city country');
+		break;
+
+		case 'Hosts':
+		case 'Executive Producer':
+		case 'EBU Scrutineer':
+		case 'Director':
+		case 'Interval Act':
+		$value = \EurovisionCrawler::split($value);
+		break;
+
+		case 'Winner':
+		$value = \EurovisionCrawler::extract($value, 'performer from country');
+		break;
+
+		case 'Qualifiers':
+		$value = $node->nextAll()->first()->filter('li.qualifiers a')->each(function (Crawler $node, $i) {
+			return $node->text();
+		});
+		break;
+
+	}
+
+	return array(
+		$key => $value,
+		);
 });
 
-print_r($details);
+$ev->details = \EurovisionCrawler::liftArray($details);
 
 // --------------------------------------------------- //
+
+$heats = $crawler->filter('.cb-EventInfo-allevents ul.events li a')->each(function (Crawler $node, $i) {
+
+	return array(
+		'heat' => $node->text(),
+		'link' => $node->link()->getUri()
+		);
+});
+
+$ev->heats = $heats;
+
+
+// --------------------------------------------------- //
+
+$scoreboard = $crawler->filter('.cb-EventInfo-scoreboard .votes_crosstable')->filter('tbody tr')->each(function (Crawler $node, $i) {
+
+	$votes = $node->filter('td:not(.participant):not(.total):not(.rank)')->each(function (Crawler $node, $i) {
+
+		$votes = \EurovisionCrawler::extract($node->attr('title'), 'pt goes to');
+
+		if ($votes->points) {
+			return array(
+				'points' => $votes->points,
+				'from' => $votes->from,
+				);
+		}
+
+		return false;
+
+	});
+
+	$votes = array_filter($votes);
+
+	$participant = trim($node->filter('td.participant .country')->text());
+	$points = intval($node->filter('td.total')->text());
+	$place = intval($node->filter('td.rank')->text());
+
+	return array(
+		'country' => $participant,
+		'votes' => $votes,
+		'points' => $points,
+		'place' => $place
+		);
+
+});
+
+$ev->scoreboard = $scoreboard;
+
+// --------------------------------------------------- //
+
+$participants = $crawler->filter('.cb-ParticipantList table.participants')->filter('tbody tr')->each(function (Crawler $node, $i) {
+
+	$order = intval($node->filter('td.ro')->text());
+	$country = trim($node->filter('td.country a')->text());
+	$broadcaster = trim($node->filter('td.country .broadcaster')->text());
+
+	$performer = $node->filter('td.country')->nextAll()->first();
+	if (count($performer->filter('.credits a'))) $profile = $performer->filter('.credits a')->link()->getUri();
+
+	$song = $node->filter('td.points')->previousAll()->first();
+	if (count($song->filter('.youtube-watch'))) $video = \EurovisionCrawler::extract($song->filter('.youtube-watch')->link()->getUri(), 'youtube');
+
+	if (stristr($song->text(), 'Watch video')) {
+		$song = \EurovisionCrawler::extract($song->text(), 'song video')->song;
+	}
+	else {
+		$song = $song->text();
+	}
+
+	$points = intval($node->filter('td.points')->text());
+	$place = intval($node->filter('td.place')->text());
+
+	return array(
+		'order' => $order,
+		'country' => $country,
+		'broadcaster' => $broadcaster,
+		'performer' => trim($performer->filter('span')->text()),
+		'link' => @$profile,
+		'song' => trim($song),
+		'youtube' => @$video->youtube,
+		'points' => $points,
+		'place' => $place,
+		);
+
+});
+
+$ev->participants = $participants;
+
+// --------------------------------------------------- //
+
+print_r($ev);
 
 echo '</pre>';
 echo 'END';
